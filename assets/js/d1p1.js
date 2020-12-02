422484287f4c6b170c351ce98002663010aa0608
dave <dave@example.com>
2020-12-02 13:29:53 +0100

    day 1 part 1&2

--- assets/js/d1p1.js
+++ assets/js/d1p1.js
@@ -1,4 +1,25 @@
-(function () {
-	const key = init();
-	console.log(key);
-})()
+init((input, cb) => {
+	const parsedInput = input.split('\n').map(val => val*1);
+	const result = getNumbersWhichAddUpToX(parsedInput, 2020, 2);
+	const resultEl = document.createElement('p');
+	resultEl.innerHTML = 'Result: ' + result.reduce((a, b) => a*b, 1);
+	cb(resultEl);
+	return;
+});
+
+function getNumbersWhichAddUpToX(listOfNumbers, targetSum, numberOfNumbers) {
+	let number;
+	while (number = listOfNumbers.shift()) {
+		if (numberOfNumbers > 1) {
+			const result = getNumbersWhichAddUpToX(listOfNumbers.slice(0), targetSum - number, numberOfNumbers - 1);
+			if (result) {
+				return result.concat(number);
+			}
+		} else {
+			if (number === targetSum) {
+				return [number];
+			}
+		}
+	}
+	return null;
+}
--- assets/js/d1p2.js
+++ assets/js/d1p2.js
@@ -1,4 +1,9 @@
-(function () {
-	const key = init();
-	console.log(key);
-})()
+init((input, cb) => {
+	const parsedInput = input.split('\n').map(val => val*1);
+	const result = getNumbersWhichAddUpToX(parsedInput, 2020, 3);
+	const resultEl = document.createElement('p');
+	console.log(result);
+	resultEl.innerHTML = 'Result: ' + result.reduce((a, b) => a*b, 1);
+	cb(resultEl);
+	return;
+});

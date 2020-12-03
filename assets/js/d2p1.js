b02289217736c7430c86036468c47d9183885281
dave <dave@example.com>
2020-12-03 09:21:33 +0100

    reworked to not used RegExp, since part 2 is kinda difficulat with RegExp ... and I want it to be similar

--- assets/js/d2p1.js
+++ assets/js/d2p1.js
@@ -1,14 +1,12 @@
 init((input, cb) => {
-	const parsedInput = input.split('\n').filter(val => val.length > 0).map(val => {
-		const retVal = {
-			regex: new RegExp(val.replace(/^(\d+)-(\d+) (.+): .*$/, '^(?:[^$3]*$3[^$3]*){$1,$2}$')),
-			value: val.replace(/^.*: (.+)$/, '$1')
-		}
-		retVal.match = retVal.regex.exec(retVal.value);
-		return retVal;
-	});
+	const validEntries = input.split('\n').filter(val => val.length > 0)
+		.map(val => val.replace(/^(\d+)-(\d+) (.+): (.*)$/, '$1\n$2\n$3\n$4').split('\n'))
+		.filter(val => {
+			const occurances = val[3].replace(new RegExp('[^' + val[2] + ']', 'g'), '').length;
+			return occurances >= val[0] && occurances <= val[1];
+		});
 	const resultEl = document.createElement('p');
-	resultEl.innerHTML = 'Result: ' + parsedInput.filter(val => val.match != null).length;
+	resultEl.innerHTML = 'Result: ' + validEntries.length;
 	cb(resultEl);
 	return;
 });

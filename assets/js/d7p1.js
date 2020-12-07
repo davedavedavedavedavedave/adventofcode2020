19e04bf09e36e686a2dd6fc1cef0e614f0dfaada
dave <dave@example.com>
2020-12-07 15:45:38 +0100

    oops, I guess performance only counts if the result is correct...

--- assets/js/d7p1.js
+++ assets/js/d7p1.js
@@ -23,24 +23,23 @@ init((input, cb) => {
 	}
 
 	const resultEl = document.createElement('p');
-	resultEl.innerHTML = 'Result: ' + (whichBagsContainBag(parsedInput, bags['shiny gold']).length - 1);
+	resultEl.innerHTML = 'Result: ' + (whichBagsContainBag(parsedInput, 'shiny gold').length - 1);
 	cb(resultEl);
 	return;
 });
 
-function whichBagsContainBag(bags, bag) {
-	const matchingBags = [bag];
+function whichBagsContainBag(bags, bagName) {
+	const matchingBags = [bagName];
 	let matchingBagsCount = 0;
 	// while new bags are added to the list of matching bags
 	while (matchingBags.length > matchingBagsCount) {
 		matchingBagsCount = matchingBags.length;
 		for (let i = 0; i < bags.length; i++) {
-			console.log(1);
 			// IF bag isn't already in list of matched bags
 			// AND it contains any of the already matched bags
-			if (matchingBags.indexOf(bags[i]) < 0 && bags[i].bag.findIndex(bag => matchingBags.indexOf(bag) > -1) > -1) {
+			if (matchingBags.indexOf(bags[i].name) < 0 && bags[i].bag.findIndex(bag => matchingBags.indexOf(bag.name) > -1) > -1) {
 				// add it to the list of matched bags
-				matchingBags.push(bags[i]);
+				matchingBags.push(bags[i].name);
 			}
 		}
 	}
